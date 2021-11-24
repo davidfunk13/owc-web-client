a9ea599d64598ac159236ad54d636e357fa76ff5
Dave Funk <davefunk135@gmail.com>
2021-11-23 22:24:22 -0700

    linetr shit and emptying out home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import * as React from "react"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import * as React from "react";
 import AppMain from "./components/AppMain/AppMain";
 import TestPage from "./views/TestPage/TestPage";
-
-
+import { useAuth0, } from "@auth0/auth0-react";
+import { BrowserRouter, Link, Route, Routes, } from "react-router-dom";
 
 export const App = () => {
-	const { isAuthenticated } = useAuth0();
-	console.log(isAuthenticated)
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<AppMain/>}>
-          <Route index element={<Home />} />
-          <Route path={"about"} element={<About />} />
-          <Route path={"dashboard"} element={<Dashboard />} />
-          <Route path={"protected"} element={<TestPage />} />
-          <Route path={"*"} element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
+	const { isAuthenticated, } = useAuth0();
+
+	console.log(isAuthenticated);
+	
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path={"/"} element={<AppMain/>}>
+					<Route index element={<Home />} />
+					<Route path={"about"} element={<About />} />
+					<Route path={"dashboard"} element={<Dashboard />} />
+					<Route path={"protected"} element={<TestPage />} />
+					<Route path={"*"} element={<NoMatch />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 function Home() {
 	return (
